3204d3305d9dcc8076a4350d18f9ae13694e1388
Julian <julyplottier@hotmail.com>
2021-10-24 04:03:09 -0300

    a

--- adapter/txtajson.py
+++ adapter/txtajson.py
@@ -1,25 +1,22 @@
-from os import close
-
-
 class TxtToJson:
 
     @staticmethod
     def converter(source, destino):
-        source = open (f'{source}','r', encoding='utf8')
-        destino = open (f'{destino}','w', encoding='utf8')
+        source = open(f'{source}', 'r', encoding='utf8')
+        destino = open(f'{destino}', 'w', encoding='utf8')
         lista_json = []
         destino.write("[")
         for line in source:
             j = line.split(';')
-
-            line_json = '{"' + 'nombre' + '"'+ ':"' + j[0] + '",' + '"' + 'apellido' + '"'+ ':"' + j[1].strip(" \n") + '"},'
+            line_json = '{"' + 'nombre' + '"' + ':"' + j[0] + '",' +\
+                        '"' + 'apellido' + '"' + ':"' +j[1].strip(" \n") + '"},'
             lista_json.append(line_json)
-
         lista_json[-1] = lista_json[-1][0:-1]
         destino.writelines(lista_json)
         destino.write(']')
         destino.close()
         source.close()
 
-if __name__== '__main__':
+
+if __name__ == '__main__':
     TxtToJson.converter('archivo.txt', 'archivo.json')

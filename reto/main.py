3a04cff47a38e62b1a8a249319da9c1a48103a10
Julian <julyplottier@hotmail.com>
2021-11-04 19:56:47 -0300

    terminado sin probar

--- reto/main.py
+++ reto/main.py
@@ -6,11 +6,11 @@ with open('lista_chica.txt', 'r', encoding='utf-8') as small_file:
 with open('lista_grande.txt', 'r', encoding='utf-8') as big_file:
     lista_grande = big_file.readlines()
 
+start = time.time()
+
 with open('lista_nueva.txt', 'w', encoding='utf-8') as new_file:
-    start = time.time()
-    for linea in lista_chica:
-        if not linea in lista_grande:
-            new_file.write(linea)
+    lista_nueva = set(lista_grande).difference(set(lista_chica))
+    new_file.write(str(lista_nueva))
 
 final = time.time() - start
 print(final)
